6d5ad9172e587e90f5d889588f20163dda3ca24d
ndubasamuel <sammynduba15@gmail.com>
2023-07-17 15:17:02 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * printing the lowercase the lowercase alphabet in reverse,
+ * main -  printing the lowercase the lowercase alphabet in reverse,
  * followed by a new line
  * Return: Always 0 (Success)
  */
@@ -15,5 +15,5 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-}
 
+}
