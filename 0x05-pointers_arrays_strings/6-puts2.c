bb0567e2ec25b06c5a206039544e608b63c6a680
ndubasamuel <sammynduba15@gmail.com>
2023-07-25 06:50:43 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character startin with the first
+ * @str: string
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int alexa = 0;
+	int b = 0;
+	char *n = str;
+	int o;
+
+	while (*n !='\0')
+	{
+		n++;
+		alexa++;
+	}
+	b = alexa - 1;
+	for (o = 0; o <= b; o++)
+	{
+		if (o %2 == 0)
+	{
+		_putchar(str[o]);
+	}
+	}
+	_putchar('\n');
+}
+
