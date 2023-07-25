043185ea8eca35c3d4139f284cce51444e7531b7
ndubasamuel <sammynduba15@gmail.com>
2023-07-25 06:23:59 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+ * print_rev prints a string in reverse 
+ * @s: string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int dev = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		dev++;
+		s++;
+	}
+	s--;
+	for (o = dev; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
