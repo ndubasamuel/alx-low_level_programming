5e0565c2271bd31c3839a2d3161b02903e260518
ndubasamuel <sammynduba15@gmail.com>
2023-08-01 07:01:21 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+/**
+ * _strstr() - finds the occurence of substring needle
+ * @hystack: string
+ * @needs: input
+ * Return: if 0 (successful)
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
+
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+
+		if (*p == '\0')
+			return (haystack);
+	}
+	return (0);
+}
