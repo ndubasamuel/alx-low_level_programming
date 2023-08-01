c28cccccb420f3125aa8ea978c9ac84d1d83cdf3
ndubasamuel <sammynduba15@gmail.com>
2023-08-01 04:50:39 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,13 +5,14 @@
  * @s: pointer memory of block to be filled
  * @b: desired value
  * @n: memory bytes
+ * Return: dest
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
 	int i;
 
-	for (i = 0; i<n; i++)
+	for (i = 0; i < n; i++)
 	{
 		*(s + i) = b;
 	}
