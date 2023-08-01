8def573cca9f53293a2011d661ccab36063ae1f1
ndubasamuel <sammynduba15@gmail.com>
2023-08-01 04:40:14 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#inlude "main.h"
+
+/**
+ * _memset - fills an outlined mamory with a constant byte 
+ * @s: pointer memory of block to be filled
+ * @b: desired value
+ * @n: memory bytes
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i<n; i++)
+	{
+		*(s + i) = b;
+	}
+
+	return (s)
+}
