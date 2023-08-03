4d5043266dbdb7ea6dd025354d7154af86ffa80e
ndubasamuel <sammynduba15@gmail.com>
2023-08-03 14:20:48 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "manin.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string to be printed
+ *
+ * Return: if 0(succefull)
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
