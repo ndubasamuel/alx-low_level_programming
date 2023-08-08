297700328e2ca809cfcdeef2082e4c6d7d6057d2
ndubasamuel <sammynduba15@gmail.com>
2023-08-08 04:42:00 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,30 +9,18 @@
  */
 int _atoi(char *s)
 {
-
 	int i, d, n, len, f, digit;
 
-
-
 	i = 0;
-
 	d = 0;
-
 	n = 0;
-
 	len = 0;
-
 	f = 0;
-
 	digit = 0;
-
-
 
 	while (s[len] != '\0')
 
 		len++;
-
-
 
 	while (i < len && f == 0)
 
@@ -41,8 +29,6 @@ int _atoi(char *s)
 		if (s[i] == '-')
 
 			++d;
-
-
 
 		if (s[i] >= '0' && s[i] <= '9')
 
@@ -63,51 +49,32 @@ int _atoi(char *s)
 				break;
 
 			f = 0;
-
 		}
 
 		i++;
-
 	}
-
-
 
 	if (f == 0)
 
 		return (0);
 
-
-
 	return (n);
-
 }
 
 
-
 /**
-
  * main - multiplies two numbers
-
  * @argc: number of arguments
-
  * @argv: array of arguments
-
  *
-
  * Return: 0 (Success), 1 (Error)
-
  */
-
 int main(int argc, char *argv[])
 
 {
 
 	int result, num1, num2;
-
-
-
 	if (argc < 3 || argc > 3)
-
 	{
 
 		printf("Error\n");
@@ -116,20 +83,13 @@ int main(int argc, char *argv[])
 
 	}
 
-
-
 	num1 = _atoi(argv[1]);
 
 	num2 = _atoi(argv[2]);
 
 	result = num1 * num2;
 
-
-
 	printf("%d\n", result);
-
-
-
 	return (0);
 
 }
