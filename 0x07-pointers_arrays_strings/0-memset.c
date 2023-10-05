2a2dc4114e6b3d57193ed08788c7367e2c55bc6d
ndubasamuel <sammynduba15@gmail.com>
2023-10-05 03:16:08 -0700

    review_changes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _memset - fills outlined mamory with a constant byte 
+ * _memset - fills outlined mamory with a constant byte
  * @s: pointer memory of block to be filled
  * @b: desired value
  * @n: memory bytes
  *
- * Return: dest
+ * Return: the number of bytes to be changed
  */
 
 char *_memset(char *s, char b, unsigned int n)
