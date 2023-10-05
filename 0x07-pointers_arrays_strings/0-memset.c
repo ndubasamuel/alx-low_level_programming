790625fa99f5642c11d9e1e19ee6fa166a34f564
ndubasamuel <sammynduba15@gmail.com>
2023-10-05 02:54:37 -0700

    low_level changes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,12 +11,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	unsigned int i;
 
-	for (; n > 0; i++)
+	for (i = 0; i < n; i++)
 	{
-		s[i] = b;
-		n--;
+		*(s + i) = b;
+
 	}
 
 	return (s);
